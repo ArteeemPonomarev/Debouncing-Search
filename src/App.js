ae96dfdcad6c94c725f943884370a38ec7ffc6b7
ArteeemPonomarev <artponomarev1994@gmail.com>
2021-10-05 13:34:09 +0300

    debounced search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
 import './App.css';
+import {useCallback, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <input type={'text'} name={'serach'} placeholder={'Enter Something...'}/>
-    </div>
-  );
+
+    const [search, setSearch] = useState([]);
+
+    const debounce = (func) => {
+
+        let timer;
+
+        return function (...args) {
+            const context = this;
+            if (timer) {
+                clearTimeout(timer)
+            }
+            timer = setTimeout(() => {
+                timer = null
+                func.apply(context, args);
+            }, 500)
+        }
+    }
+
+
+
+    const handleChange = (event) => {
+        const {value} = event.target;
+         fetch(`https://demo.dataverse.org/api/search?q=${value}`)
+             .then(res => res.json())
+             .then(json => setSearch(json.data.items))
+             .catch(error => setSearch([error]))
+    }
+
+
+    const optimisedVersion = useCallback(debounce(handleChange), [])
+
+    return (
+        <div className="App">
+            <input type={'text'} name={'serach'} placeholder={'Enter Something...'}
+                   className={'search'} onChange={optimisedVersion}
+            />
+            {search.length > 0 &&
+            <div className={'autocomplete'}>
+                {search?.map((el,i) =>
+                    <div kei={i} className={'autocompleteItems'}>
+                        <span>{el.name}</span>
+                    </div>
+                )}
+            </div>
+            }
+        </div>
+    );
 }
 
 export default App;
